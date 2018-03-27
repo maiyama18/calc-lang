392bd5525d065bbdd1feb490663df228f8b75e9a
ymr27 <myam0118@gmail.com>
2018-03-27 18:24:29 +0900

    simplify lexer

--- src/lexer.js
+++ src/lexer.js
@@ -1,14 +1,13 @@
 const NUMBER_REGEX = "\\d+\\.?\\d*";
 const OPERATOR_REGEX = "[+\\-*\\/%]";
-const PUNCTUATION_REGEX = "[();]";
 
-const REGEX = `(${NUMBER_REGEX})|(${OPERATOR_REGEX})|(${PUNCTUATION_REGEX})`;
+const REGEX = `(${NUMBER_REGEX})|(${OPERATOR_REGEX})`;
 
 const lex = input => {
   let tokens = [];
-  let start = 0;
+  let position = 0;
 
-  let matched = input.slice(start).match(REGEX);
+  let matched = input.slice(position).match(REGEX);
   while (matched) {
     if (matched[1]) {
       tokens.push({
@@ -20,22 +19,16 @@ const lex = input => {
         type: 'operator',
         value: matched[2],
       });
-    } else if (matched[3]) {
-      tokens.push({
-        type: 'punctuation',
-        value: matched[3],
-      });
     } else {
       throw new Error('invalid token!' + matched[0]);
     }
 
-    start += matched.index + matched[0].length;
-    matched = input.slice(start).match(REGEX);
+    position += matched.index + matched[0].length;
+    matched = input.slice(position).match(REGEX);
   }
 
   tokens.push({
-    type: 'punctuation',
-    value: 'EOL',
+    type: 'end',
   });
   return tokens;
 };
--- test/test_lexer.js
+++ test/test_lexer.js
@@ -3,43 +3,57 @@ const { lex } = require('../src/lexer');
 
 describe('lex', () => {
   it('should lex empty input', () => {
-    assert.deepStrictEqual(lex(' '), [{ type: 'punctuation', value: 'EOL' }]);
+    assert.deepStrictEqual(
+        lex(' '),
+        [{ type: 'end' }]
+    );
   });
 
-  it('should lex number', () => {
-    assert.deepStrictEqual(lex('3'), [
-      { type: 'number', value: 3 },
-      { type: 'punctuation', value: 'EOL' },
-    ]);
+  it('should lex integer', () => {
+    assert.deepStrictEqual(
+        lex('3'),
+        [
+          { type: 'number', value: 3 },
+          { type: 'end' },
+        ]);
   });
 
-  it('should lex operator/number', () => {
+  it('should lex float', () => {
+    assert.deepStrictEqual(
+        lex('4.75'),
+        [
+          { type: 'number', value: 4.75 },
+          { type: 'end' },
+        ]);
+  });
+
+  it('should lex unary operator', () => {
     assert.deepStrictEqual(lex('-5'), [
       { type: 'operator', value: '-' },
       { type: 'number', value: 5 },
-      { type: 'punctuation', value: 'EOL' },
+      { type: 'end' },
     ]);
   });
 
-  it('should lex number/operator/number', () => {
-    assert.deepStrictEqual(lex('3 * 7'), [
-      { type: 'number', value: 3 },
+  it('should lex binary operator', () => {
+    assert.deepStrictEqual(lex('3.3 * 7'), [
+      { type: 'number', value: 3.3 },
       { type: 'operator', value: '*' },
       { type: 'number', value: 7 },
-      { type: 'punctuation', value: 'EOL' },
+      { type: 'end' },
     ]);
   });
 
-  it('should lex punctuation', () => {
-    assert.deepStrictEqual(lex('(4 + 2) * 7'), [
-      { type: 'punctuation', value: '(' },
+  it('should lex equation with more than one operators', () => {
+    assert.deepStrictEqual(lex('4 + 2 * 7 / 3'), [
       { type: 'number', value: 4 },
       { type: 'operator', value: '+' },
       { type: 'number', value: 2 },
-      { type: 'punctuation', value: ')' },
       { type: 'operator', value: '*' },
       { type: 'number', value: 7 },
-      { type: 'punctuation', value: 'EOL' },
+      { type: 'operator', value: '/' },
+      { type: 'number', value: 3 },
+      { type: 'end' },
     ]);
   });
 });
